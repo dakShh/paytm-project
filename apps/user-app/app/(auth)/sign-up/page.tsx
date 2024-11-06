d0bd7aeeb8754eb98cac56cd9892c76f154dd1b7
dakShh <daksh.dk207@gmail.com>
2024-11-06 11:34:17 -0500

    signup page setup, fixed folder structure for user-app

---
+++ apps/user-app/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+export default function SignUp() {
+    return (
+        <div className="container mx-auto grid place-items-center h-screen ">
+            <div className="w-96 p-5 bg-white  rounded-lg shadow-lg">Signup</div>
+        </div>
+    );
+}
